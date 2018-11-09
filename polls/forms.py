6fa97968343bb14c96c995006d3f817dbec91709
xingfuggz <2539909370@qq.com>
2018-11-09 10:47:23 +0800

    实现最终添加功能

---
+++ polls/forms.py
@@ -0,0 +1,18 @@
+# from django import forms
+from django.forms import ModelForm, Textarea
+from .models import Topic, Entry
+
+class TopicForm(ModelForm):
+	class Meta:
+		model = Topic
+		fields = ['text']
+		labels = {'text':'提交'}
+
+class EntryForm(ModelForm):
+	class Meta:
+		model = Entry
+		fields = ['text']
+		labels = {'text':'添加笔记'}
+		widgets = {
+			'text':Textarea(attrs={'cols':80, 'rows':20}),
+		}
